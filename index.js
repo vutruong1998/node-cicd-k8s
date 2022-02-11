f108aeeff29457d00bbbeabfa6b117c68ce8a58e
Truong Vu <truong@ltv.dev>
2022-02-11 22:39:35 +0700

    test redis

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { createClient } = require('redis');
+const uuid = require('uuid')
+
+const client = createClient()
+client.on('error', (err) => console.log('Redis Client Error', err))
+client.connect()
 
 app.get('/', (req, res) => {
   res.send('Nodejs - CI/CD - K8S')
@@ -8,6 +14,16 @@ app.get('/', (req, res) => {
 
 app.get('/ready', (req, res) => {
   res.send('Ready')
+})
+
+app.get('/call-redis', async (req, res) => {
+  const key = uuid.v4()
+
+  await client.set(key, `dcmm ${key}`)
+
+  const value = await client.get(key)
+
+  res.send(value)
 })
 
 app.listen(port, () => {
